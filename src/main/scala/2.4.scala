d054c91c6037b442ad6635bbea314bd8f2fcb633
Jiawei Wang <wangjiawei_med@hotmail.com>
2023-08-17 00:18:32 -0400

    2.4 and 2.5

--- src/main/scala/2.4.scala
+++ src/main/scala/2.4.scala
@@ -1,3 +1,8 @@
 // a script is just a scala source file that includes a top level function annotated as @main
-@main def m() = 
-    println("hello")
+// @main def m() = 
+//     println("hello")
+
+// an example of input reading
+@main def m(args: String*) = {
+    println("hello, " + args(0))
+}
