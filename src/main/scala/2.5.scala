2a626bedca097a41bd649ac9f5049b2e5d689279
Jiawei Wang <wangjiawei_med@hotmail.com>
2023-08-19 23:02:32 -0400

    finish charpter 2

--- src/main/scala/2.5.scala
+++ src/main/scala/2.5.scala
@@ -1,8 +1,29 @@
-// loop with while
-@main def m(args: String*) = {
-    var i = 0
-    while i < args.length do {
-        println(args(i))
-        i += 1
-    }
-}
+// loop with while, not a good approach in scala
+// @main def m(args: String*) = {
+    // var i = 0 // auto type inference
+    // while i < args.length do {
+        // println(args(i))
+        // i += 1 // no i++ or ++i in scala
+    // }
+// }
+
+// if statement, this example shows parentheses, curly braces and semicolons are optional
+// @main def m(args: String*) = 
+    // var i = 0
+    // while i < args.length do 
+        // if i != 0 then print(" ") // print a space after every input, nothing before the first input
+        // print(args(i)) // no new line
+        // i += 1
+    // println("\nfinish")
+
+// iterate with foreach and for-do, a more functional style than imperative 
+// @main def m(args: String*) =
+    // args.foreach((arg: String) => println(arg)) 
+    // 1. function literal (lambda expression)
+    // 2. if type is mentioned, argument portion needs to be within parentheses
+
+// syntax of a function literal: parameters => function body
+// (x: Int, y: Int) => x + y
+
+// if a function literal has one statement that takes one argument, it can be shortened as
+@main def m(args: String*) = args.foreach(println)
